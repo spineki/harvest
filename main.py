32e6f6bf47a9825061999b3595c26bce3fccafab
Marras Antoine <antoine.marras@student-cs.fr>
2020-12-04 10:51:18 +0100

    added comments

--- main.py
+++ main.py
@@ -1,55 +1,83 @@
 import os
+from typing import List
 
 
-def splitter(filename, MAX_SIZE=7):
+def splitter(file_name: str, MAX_SIZE: int = 7):
     """
-    filename to split: 
-    MAX_SIZE en MO: default 7, adapted for discord
+    Take file_name as input an split it in chucks of size MAX_SIZE. 
+
+    file_name to split: 
+    MAX_SIZE: MB size: default 7 MB
     """
 
+    # convertion to MB
     MAX_SIZE = MAX_SIZE * 1024 * 1024
 
-    start_index = 0
-    
-    data = None
+    # index go throught the bit stream
+    start_index: int = 0
 
-    nb_fichier = 0
+    # harvested data
+    data: bytes = None
 
-    with open(filename, "rb") as fin:
+    created_files: int = 0
+
+    with open(file_name, "rb") as input_stream:
+        # while we didn't go out the file
         while data != b'':
-            fin.seek(start_index)
-            data = fin.read(MAX_SIZE)
+            # we place the cursor at start index
+            input_stream.seek(start_index)
+            # read a chunk of size MAX_SIZE bytes
+            data = input_stream.read(MAX_SIZE)
 
             if data == b'':
                 break
+            # then we open an output file
+            with open(str(start_index) + "_" + file_name, "wb") as ouput_stream:
+                # A write the related chunk in it
+                ouput_stream.write(data)
 
+            created_files += 1
+
+            # we translate the cursor
             start_index += MAX_SIZE
 
-            with open(str(start_index) + "_" + filename, "wb") as ouput:
-                ouput.write(data)
-            nb_fichier += 1
-    print("Finis!! en ", nb_fichier, " fichiers")
+    print("Done! ", created_files, " files created")
 
 
-def binder(folder_name, output_name="output.exe"):
-    files = os.listdir(folder_name)
+def binder(folder_name: str, output_name: str = "output.exe", verbose=True):
+    """
+    Merge all the files from folder_name into a file output_name. Display additional data if verbose=True
+    """
 
+    # we get all the files from the given  folder
+    files: List[str] = os.listdir(folder_name)
+
+    if files == []:
+        print(" No file in ", folder_name, " folder")
+        return
+
+    # we sort then by comparing the concatenated number
     files = sorted(files, key=lambda x: int(x.split("_")[0]))
 
-    for file in files:
-        print(file)
+    if verbose:
+        print("encoutered {} files:".format(len(files)))
+        for file in files:
+            print(file)
 
-    with open(output_name, "wb+") as output:
-
+    # we open an output stream
+    with open(output_name, "wb+") as output_stream:
+        # And for every gathered files
         for file in files:
             with open(os.path.join(folder_name, file), "rb") as input:
-                output.write(input.read())
-    print("Fini!")
+                # we add it at the end of the document
+                output_stream.write(input.read())
+
+    print("Done!")
 
 
 if __name__ == "__main__":
     # Programme principal. Retirer le # devant la ligne pour lancer la commande
 
     # splitter("Votre nom de fichier avec l'extension")
-    # binder("mon nom de dossier", "ouput.exe")
+    # binder("mon nom de dossier", "ouput.exe", verbose=True)
     pass
